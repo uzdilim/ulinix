a87dd8aae7458e23db6721448101bfefc8be2126
uzdilim <95263010+uzdilim@users.noreply.github.com>
2021-12-22 13:51:33 +0800

    Create foot.js

---
+++ js/foot.js
@@ -0,0 +1,4 @@
+document.writeln("<footer class=\'footer-site\'>");
+document.writeln("<p class=\'links\' style=\'font-size: 14px\' ><a href=\'http://www.uzdil.com/\'>توربەت ھوقۇقى ئۇزدىل تورى غا تەۋە</a></p>");
+document.writeln("<p class=\'ex\' style=\'font-size: 14px\'>Copyright Uzdil.com All Rights Reserved</p>");
+document.writeln("</footer>");
